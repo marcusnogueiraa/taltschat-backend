01b46dec35a1b01e05939ad467181b9c96a84ebc
Marcus Nogueira <marcusnogueiraa@gmail.com>
2024-12-23 17:45:43 -0300

    init: new project

---
+++ src/main/java/com/marcusnogueiraa/taltschat/TaltschatApplication.java
@@ -0,0 +1,13 @@
+package com.marcusnogueiraa.taltschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaltschatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaltschatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcusnogueiraa/taltschat/TaltschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcusnogueiraa.taltschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaltschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
